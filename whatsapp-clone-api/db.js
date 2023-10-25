9a9c28ed622cd80d2e6a3d323f783357cd3616ad
rezaalih121 <reza-ali.irannejad@eesc54.fr>
2023-10-25 18:14:54 +0200

    CROS origin added to index.js and it is working 😊👌

--- whatsapp-clone-api/db.js
+++ whatsapp-clone-api/db.js
@@ -17,9 +17,9 @@ module.exports.Userw = UserwModel
 module.exports.init = async()=>{
     await mongoose.connect(uri_db)
     await UserwModel.deleteMany({})
-    await new UserwModel({name:'Neo' , phoneNumber:'121777', avatarImage:'http://www.avatarstock.com/img/matrix-avatar-1487_15827.jpg', friendsList:["121777","121110","121121"]}).save()
-    await new UserwModel({name:'Trinity' , phoneNumber:'121110' , avatarImage:'http://2.bp.blogspot.com/-pFh5cVd7mlI/UgQnNeh335I/AAAAAAAAM1s/6Io8KEtQGGA/s180/Trinity.jpg',friendsList:["121777","121110","121121"]}).save()
-    await new UserwModel({name:'Morpheus' , phoneNumber:'121121' , avatarImage:'https://www.listchallenges.com/f/items/18b1a46f-36c9-4af4-aedd-faae3fcfed4b.jpg',friendsList:["121777","121110","121121"]}).save()
-    await new UserwModel({name:'Smith' , phoneNumber:'666' , avatarImage:'https://www.monologuedb.com/wp-content/uploads/2011/04/Hugo-Weaving-Agent-Smith-The-Matrix-150x150.gif', friendsList:["121777"]}).save()
+    await new UserwModel({name:'Neo' , phoneNumber:'121777', avatarImage:'http://www.avatarstock.com/img/matrix-avatar-1487_15827.jpg', friendsList:["121777","121110","121121"],socketId:""}).save()
+    await new UserwModel({name:'Trinity' , phoneNumber:'121110' , avatarImage:'http://2.bp.blogspot.com/-pFh5cVd7mlI/UgQnNeh335I/AAAAAAAAM1s/6Io8KEtQGGA/s180/Trinity.jpg',friendsList:["121777","121110","121121"],socketId:""}).save()
+    await new UserwModel({name:'Morpheus' , phoneNumber:'121121' , avatarImage:'https://www.listchallenges.com/f/items/18b1a46f-36c9-4af4-aedd-faae3fcfed4b.jpg',friendsList:["121777","121110","121121"],socketId:""}).save()
+    await new UserwModel({name:'Smith' , phoneNumber:'666' , avatarImage:'https://www.monologuedb.com/wp-content/uploads/2011/04/Hugo-Weaving-Agent-Smith-The-Matrix-150x150.gif', friendsList:["121777"],socketId:""}).save()
     console.log('db is ready');
 }
--- whatsapp-clone-api/index.js
+++ whatsapp-clone-api/index.js
@@ -8,33 +8,39 @@ const Server =  require('socket.io')
 const cors = require('cors');
 const { log } = require('console');
 const io = Server(server , 
-       { 
-            cors: {
-                origin:'http://localhost',
-                methods:['GET' , 'POST']
-            }
+    { 
+        cors: {
+            origin:'http://localhost',
+            methods:['GET' , 'POST']
         }
+    }
     )
-db.init()
-
-app.use(cors())
-app.use(express.json())
-
-server.listen(3000 ,() => {
-    console.log('Listen on 3000');
-});
-
-app.get('/userw/:phoneNumber' , async (req,res) => {
+    db.init()
     
-    const userw = await db.Userw.findOne({phoneNumber:req.params.phoneNumber}).exec()
-    console.log(userw + '=====>>>'+req.params.phoneNumber);
-    res.send(userw)
-});
+    app.use(cors())
+    app.use(express.json())
+    
+    server.listen(3000 ,() => {
+        console.log('Listen on 3000');
+    });
+    
+    app.get('/userw/:phoneNumber' , async (req,res) => {
+        
+        const userw = await db.Userw.findOne({phoneNumber:req.params.phoneNumber}).exec()
+        console.log(userw + '=====>>>'+req.params.phoneNumber);
+        res.send(userw)
+    });
+    
+    app.get('/userw', () => {
+        const users = db.Userw.find({}).exec(); 
+        res.send(users);
+    });
 
-app.put('/userw',async (req,res)=>{
+    app.put('/userw',async (req,res)=>{
     const result = await db.Userw.updateOne({phoneNumber:req.body.phoneNumber},{socketId:req.body.socketId})
     res.send(result)
 })
+
 
 io.on('connection' , (socket)=>{
     socket.on('messages',(data)=>{
